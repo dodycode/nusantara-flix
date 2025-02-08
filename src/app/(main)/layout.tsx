73f032ee00336f77d93aae208187fbf717ac1aa6
Dody <prasetyodody17@gmail.com>
2025-02-08 23:22:50 +0000

    feat: bump deps and remove unused codes

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,28 +1,12 @@
-import { ConfirmationProvider } from "@/components/ui/confirmation-dialog";
-import { ThemeToggle } from "@/components/ui/theme";
-import { SessionProvider } from "@/contexts/session-provider";
-import { ensureAuth } from "@/lib/auth";
-
-import { Navbar } from "./_components/navbar";
-
 type Props = {
   children: React.ReactNode;
 };
 
-export default async function MainLayout({ children }: Props) {
-  const session = await ensureAuth();
-
+export default function MainLayout({ children }: Props) {
   return (
-    <SessionProvider initialData={session}>
-      <ConfirmationProvider>
-        <Navbar />
-        <div className="mx-auto mt-36 min-h-[calc(100dvh-220px)] max-w-6xl px-4 lg:px-0">
-          {children}
-        </div>
-        <div className="sticky bottom-2 flex flex-col items-end p-5">
-          <ThemeToggle />
-        </div>
-      </ConfirmationProvider>
-    </SessionProvider>
+    <div className="flex h-screen flex-col overflow-hidden font-sans">
+      <div className="flex-none">Nav here</div>
+      <div className="h-full flex-1 overflow-y-auto">{children}</div>
+    </div>
   );
 }
