50a110dd9c5fd727acc918f46741b712cafdcecc
Dody <prasetyodody17@gmail.com>
2025-02-09 05:47:52 +0000

    feat: design

--- src/app/(main)/people/[id]/page.tsx
+++ src/app/(main)/people/[id]/page.tsx
@@ -1,9 +1,11 @@
+import { PeopleCredits } from "./_components/people-credits";
 import { PeopleInfo } from "./_components/people-info";
 
 export default function PersonDetails() {
   return (
     <div className="flex flex-col">
       <PeopleInfo />
+      <PeopleCredits />
     </div>
   );
 }
