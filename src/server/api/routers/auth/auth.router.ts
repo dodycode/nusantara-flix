b5f69538f0e0b70df5e2d0de80994f6310dd22df
Dody <prasetyodody17@gmail.com>
2025-02-09 13:55:09 +0000

    fix: typecheck

--- src/server/api/routers/auth/auth.router.ts
+++ src/server/api/routers/auth/auth.router.ts
@@ -1,7 +1,10 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { auth } from "@/server/auth";
 
 export const authRouter = createTRPCRouter({
-  getSession: publicProcedure.query(({ ctx }) => {
-    return ctx.session;
+  getSession: publicProcedure.query(async () => {
+    const session = await auth();
+
+    return session;
   }),
 });
